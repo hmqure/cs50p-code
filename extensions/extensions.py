fca266ac63d68a1095e0bd8e39aca6e17ebc85bc
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-10 03:05:26 +0000

    Fri, Jun 10, 2022, 3:05 AM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -4,21 +4,28 @@ extensions = []
 
 images = ['jpeg', 'png', 'gif']
 images2 = ['jpg']
-apps = ['pdf', 'txt', 'zip']
+text = ['txt']
+apps = ['pdf', 'zip']
 lower = str.lower()
 lower = lower.replace(" ", "")
 
 double = []
+front = []
 
 for i in range(len(lower)):
     if lower[i] == '.':
-         double.append(lower[i+1::])
+        double.append(lower[i+1::])
+        front.append(lower[0:i])
+
 
 if double[-1] in images:
     print("image/"+double[-1])
 elif double[-1] in images2:
     print("image/jpeg")
+elif double[-1] in text:
+    print("text/"+front[0])
 elif double[-1] in apps:
     print("application/"+double[-1])
 else:
     print("application/octet-stream")
+
