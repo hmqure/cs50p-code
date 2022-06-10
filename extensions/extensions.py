6ce4a1f2387610923588cb48bb8e0f1adaa845bb
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-10 02:33:56 +0000

    Fri, Jun 10, 2022, 2:33 AM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -7,15 +7,37 @@ apps = ['pdf', 'txt', 'zip']
 lower = str.lower()
 lower = lower.replace(" ", "")
 
+double = []
+
 for i in range(len(lower)):
-    if lower[i] == '.':
-        ext = lower[i+1::]
-        if ext in images:
-            print("image/"+ext)
-        elif ext in apps:
-            print("application/"+ext)
-        else:
-            print("application/octet-stream")
+    count = lower.count(".")
+    if count == 1:
+        if lower[i] == '.':
+            ext = lower[i+1::]
+            if ext in images:
+                print("image/"+ext)
+            elif ext in apps:
+                print("application/"+ext)
+            else:
+                print("application/octet-stream")
+    if count != 1:
+        if lower[i] == '.':
+            double.append(lower[i+1::])
+
+
+if double is not False:
+    if double[-1] in images:
+        print("image/"+double[-1])
+    elif double[-1] in apps:
+        print("application/"+double[-1])
+    else:
+        print("application/octet-stream")
+else:
+    pass
+
+
+
 
 if "." not in lower:
     print("application/octet-stream")
+         
