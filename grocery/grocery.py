18a1be81b90802c5abae44e640d32ba2d5cbe306
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-11 22:58:23 +0000

    Sat, Jun 11, 2022, 10:58 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,19 +1,27 @@
-items = ['apple', 'apple', 'banana']
+items = []
 finals = []
 ff = []
 
 while True:
     try:
+
         item = input("Enter item:")
         upper = item.upper()
         items.append(upper)
+
     except EOFError:
+
+        for i in items:
+            finals.append(f'{items.count(i)} {i}')
+
+        for i in finals:
+            if i not in ff:
+                ff.append(i)
+
         for i in ff:
             print(i)
-        break
 
-for i in items:
-    finals.append(f'{items.count(i)} {i}')
-for i in finals:
-    if i not in ff:
-        ff.append(i) 
+        for i in ff:
+            print(i)
+
+        break
