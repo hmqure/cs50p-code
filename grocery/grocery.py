a07ad8a22402d1f901479ffc00043aa5f4812cbe
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-11 22:55:05 +0000

    Sat, Jun 11, 2022, 10:55 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -7,13 +7,13 @@ while True:
         item = input("Enter item:")
         upper = item.upper()
         items.append(upper)
-        for i in items:
-            finals.append(f'{items.count(i)} {i}')
-        for i in finals:
-            if i not in ff:
-                ff.append(i)
-
     except EOFError:
-        pass
         for i in ff:
             print(i)
+        break
+
+for i in items:
+    finals.append(f'{items.count(i)} {i}')
+for i in finals:
+    if i not in ff:
+        ff.append(i) 
