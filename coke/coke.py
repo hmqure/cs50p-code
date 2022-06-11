12069ff2fa4d6e1adf6e5862ce7bfe11b4a68d9f
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-11 04:29:51 +0000

    Sat, Jun 11, 2022, 4:29 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,11 +1,15 @@
 x = 50
 print("Amount Due", x)
+coins = ['25', '10', '5']
 
 while x != 0:
     coin = input("Insert Coin:")
-    x = x - int(coin)
-    if x > 0:
+    if coin in coins:
+        x = x - int(coin)
+        if x > 0:
+            print("Amount Due", x)
+        elif x <= 0:
+            print("Change Owed:", x*-1)
+            break
+    else:
         print("Amount Due", x)
-    elif x <= 0:
-        print("Change Owed:", x*-1)
-        break
