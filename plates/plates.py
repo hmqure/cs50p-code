e83f18efc856e0aa2bdfae348ea5bb9470bef72b
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-11 03:09:50 +0000

    Sat, Jun 11, 2022, 3:09 AM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,13 +8,20 @@ def main():
 
 def is_valid(s):
     zero = []
-    if 2 <= len(s) <= 6:
+    num = ''
+    if 2 <= len(s) <= 6 and s.isalpha() is not True:
         if s[0:2].isalpha() is True:
             if s.isalnum() is True:
                 for i in range(len(s)):
                     if s[i].isdigit() is True:
                         zero.append(s[i])
-                        if zero[0] != '0' and s[-1].isnumeric() is True:
-                            return True
+                        if s[i].isdigit() is True:
+                            num += s[i::]
+                            if num.isdigit() is True:
+                                if zero[0] != '0' and s[-1].isnumeric() is True:
+                                    return True
+
+    elif 2 <= len(s) <= 6 and s.isalpha() is True:
+        return True
 
 main()
