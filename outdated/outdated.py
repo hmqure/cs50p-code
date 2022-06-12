1027de01d70e9de2bb92efdd99f939939d1bc08e
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-12 07:02:56 +0000

    Sun, Jun 12, 2022, 7:02 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -22,26 +22,30 @@ while True:
         for i in split:
             if i == "":
                 split.remove(i)
+        if split[1].isdigit() is True and split[2].isdigit() is True:
+            if 1 <= int(split[1]) <= 31:
 
-        if 1 <= int(split[1]) <= 31:
+                if split[0].isdigit() is False:
+                    for i in range(len(months)):
+                        if months[i] == split[0]:
+                            print((f"{int(split[2]):04}")+"-"+(f"{i+1:02d}")+"-"+(f"{int(split[1]):02d}"))
+                            broken = True
+                            break
 
-            if split[0].isdigit() is False:
-                for i in range(len(months)):
-                    if months[i] == split[0]:
-                        print((f"{int(split[2]):04}")+"-"+(f"{i+1:02d}")+"-"+(f"{int(split[1]):02d}"))
+                elif split[0].isdigit() is True:
+                    if 1 <= int(split[0]) <= 12:
+                        print((f"{int(split[2]):04}")+"-"+(f"{int(split[0]):02d}")+"-"+(f"{int(split[1]):02d}"))
                         broken = True
                         break
+                    else:
+                        pass
 
-            elif split[0].isdigit() is True:
-                if 1 <= int(split[0]) <= 12:
-                    print((f"{int(split[2]):04}")+"-"+(f"{int(split[0]):02d}")+"-"+(f"{int(split[1]):02d}"))
-                    broken = True
-                    break
-                else:
-                    pass
+            else:
+                pass
 
         else:
-            pass
+            break
 
     except EOFError as e:
         pass
+
