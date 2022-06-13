4d85c08449e7480eae1cabc347af03c2ad5b3568
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-13 05:39:09 +0000

    Mon, Jun 13, 2022, 5:39 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -9,18 +9,18 @@ x = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
 if len(sys) == 2:
     if sys[1].isdigit() is True:
-        try:
             y = x.json()
             rate = y["bpi"]['USD']['rate']
             newrate = rate.replace(',','')
             total = float((float(newrate))*float(sys[1]))
-            print("$"+"{:,}".format(total))
+            print("$"+"{:,.4f}".format(total))
 
-
-
-        except requests.RequestException:
-            sys.exit("Psych")
-
+    elif "." in str(floatsys):
+        y = x.json()
+        rate = y["bpi"]['USD']['rate']
+        newrate = rate.replace(',','')
+        total = float((float(newrate))*floatsys)
+        print("$"+"{:,.4f}".format(total))
 
     else:
         sys.exit("Command-line argument is not a number")
