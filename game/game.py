af67afd9eddc3ffa63349a592534fa86b0eeff3d
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-13 04:28:26 +0000

    Mon, Jun 13, 2022, 4:28 AM +00:00

--- game/game.py
+++ game/game.py
@@ -1,5 +1,40 @@
 import random
 
-while True:
+counter = 0
 
-num = intput(")
+while counter == 0:
+
+    level = input("Level:")
+
+    if level.isdigit() is True:
+
+        if int(level) >= 1:
+            print(level)
+
+            while True:
+
+                guess = input("Guess:")
+                num = random.randint(1,int(level))
+
+                if int(guess) == num:
+                    print('Just right!')
+                    counter = counter + 1
+                    break
+
+                elif int(guess) > num:
+                    print("Too large!")
+                    pass
+
+                elif int(guess) < num:
+                    print("Too small!")
+                    pass
+
+                else:
+                    pass
+
+        else:
+            pass
+
+    else:
+        pass
+
