1872d87355a802fe3c3fbfc887d750d50a75c13e
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-13 05:26:39 +0000

    Mon, Jun 13, 2022, 5:26 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -7,7 +7,7 @@ sys = sys.argv
 x = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
 if len(sys) == 2:
-    if sys[1].isdigit() is True:
+    if sys[1].isdigit() is True or sys[1].isdigit() is True:
         try:
             y = x.json()
             rate = y["bpi"]['USD']['rate']
