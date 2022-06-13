681f75191b87e5dd93285be4cf9b8eb29acde80f
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-13 04:55:38 +0000

    Mon, Jun 13, 2022, 4:55 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,12 +4,15 @@ import json
 
 sys = sys.argv
 
+x = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+
 if len(sys) == 2:
     if sys[1].isdigit() is True:
-
         try:
-            print(request.get())
+            y = json.dumps(x)
+            print(y)
         except requests.RequestException:
+            sys.exit("Psych")
 
 
     else:
