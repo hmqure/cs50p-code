81ed8b313e5e9e701c80b3f4de6b5e2691e80dda
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-13 01:01:23 +0000

    Mon, Jun 13, 2022, 1:01 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,20 +6,20 @@ s = "String"
 
 figlet = Figlet()
 figlet.getFonts()
-figlet.setFont(font=f)
-print(figlet.renderText(s))
+# figlet.setFont(font=f)
+# print(figlet.renderText(s))
 
-# try:
-#     if f != "":
-#         split = f.split(" ")
-#         if split[0] == "-f" or split[0] == "--font":
-#             string = input()
-#             font1 = split[1]
-#             figlet.setFont(font=font1)
-#             print(figlet.renderText(string))
+try:
+    if f != "":
+        split = f.split(" ")
+        if split[0] == "-f" or split[0] == "--font":
+            string = input()
+            font1 = split[1]
+            figlet.setFont(font=font1)
+            print(figlet.renderText(string))
 
-# except EOFError as e:
-#     print(e)
+except EOFError as e:
+    print(e)
 
     # elif f == "":
     #     string = input()
