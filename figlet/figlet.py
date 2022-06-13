5d05d8a5ea3b27ded811612349e2b391fd4344f0
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-13 02:19:13 +0000

    Mon, Jun 13, 2022, 2:19 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,31 +1,28 @@
 from pyfiglet import Figlet
 import random
 import sys
+
 sys = sys.argv
 ok = ['-f', '--font']
 figlet = Figlet()
 force = figlet.getFonts()
 
 
-try:
-
-    if len(sys) == 3 and sys[1] in ok:
-        font1 = sys[2]
-        if font1 in force:
-            figlet.setFont(font=font1)
-            string = input()
-            print(figlet.renderText(string))
-
-        else:
-            sys.exit("Invalid usage")
-
-    elif len(sys) == 1:
+if len(sys) == 3 and sys[1] in ok:
+    font1 = sys[2]
+    if font1 in force:
+        figlet.setFont(font=font1)
         string = input()
-        figlet.setFont(font=random.choice(force))
         print(figlet.renderText(string))
 
     else:
         sys.exit("Invalid usage")
 
-except EOFError as e:
-    print(e)
+elif len(sys) == 1:
+    string = input()
+    figlet.setFont(font=random.choice(force))
+    print(figlet.renderText(string))
+
+else:
+    sys.exit("Invalid usage")
+
