c17e59ce202e33879cb86733775589f7c299dff6
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-13 00:39:08 +0000

    Mon, Jun 13, 2022, 12:39 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,12 +1,12 @@
 from pyfiglet import Figlet
-
+import random
+import sys
 f = input()
 s = "String"
 
 try:
-    if
+    if f[0:2]
     figlet = Figlet()
-
     figlet.getFonts()
 
     figlet.setFont(font=f)
