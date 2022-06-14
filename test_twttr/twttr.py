d507e022181d1c4fb8ea7ea48465c4765ad32883
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-14 07:46:28 +0000

    Tue, Jun 14, 2022, 7:46 AM +00:00

---
+++ test_twttr/twttr.py
@@ -0,0 +1,20 @@
+def main():
+    enter = input("Input:")
+    print(shorten(enter))
+
+
+def shorten(word):
+    new = ''
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    lower = word.lower()
+
+    for i in range(len(lower)):
+        if lower[i] not in vowels:
+            new += lower[i]
+
+    return new
+
+
+if __name__ == "__main__":
+    main()
