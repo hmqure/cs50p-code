c1776d89c83ac6df554b04b2e7bc3c11d0a38b8a
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 04:09:13 +0000

    Wed, Jun 15, 2022, 4:09 AM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -4,8 +4,8 @@ def main():
     test_len()
 
 def test_len():
-    assert is_valid("G") is False
-    assert is_valid("AAAAAAAAAA") is False
+    assert is_valid("G") is not True
+    assert is_valid("AAAAAAAAAA") is not True
     assert is_valid("HELLO") is True
     assert is_valid("CS50") is True
 
