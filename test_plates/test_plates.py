3bb03dc15b6cc77adf7b997b8635bd6c07aed417
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 03:06:49 +0000

    Wed, Jun 15, 2022, 3:06 AM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,15 +1,13 @@
 from plates import is_valid
 
 def main():
-    test_length()
+    test_len()
 
-def test_length():
-    assert is_valid("G") is
-
-     False
-    # assert is_valid("AAAAAAAAAA") == "Invalid"
-    # assert is_valid("HELLO") == "Valid"
-    # assert is_valid("CS50") == "Valid"
+def test_len():
+    assert is_valid("G") is False
+    assert is_valid("AAAAAAAAAA") is False
+    assert is_valid("HELLO") is True
+    assert is_valid("CS50") is True
 
 if __name__ == "__main__":
     main()
