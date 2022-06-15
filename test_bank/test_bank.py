20c56b99134f09176478369a9b01a610bbbccdbc
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 00:56:39 +0000

    Wed, Jun 15, 2022, 12:56 AM +00:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -3,15 +3,18 @@ from bank import value
 def main():
     test_value()
 
-def test_value():
+def test_hello():
     assert value("Hello") == "$0"
     assert value("hello") == "$0"
+
+def test_h():
     assert value("hey") == "$20"
     assert value("Hopper") == "$20"
-    assert value("Hello") == "$0"
-    assert value("Hello") == "$0"
 
-
+def test_non():
+    assert value("very") == "$100"
+    assert value("3453") == "$100"
+    assert value("?>/.") == "$100"
 
 if __name__ == "__main__":
     main()
