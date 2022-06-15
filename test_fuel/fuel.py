bc08a73081aea46b7c444b8f1a44cf3d87c8c654
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 05:26:51 +0000

    Wed, Jun 15, 2022, 5:26 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -5,7 +5,7 @@ def main():
 def convert(fraction):
      n = 0
      try:
-        split = frac.split("/")
+        split = fraction.split("/")
         n = n + (round(((int(split[0]))/(int(split[1])))*100))
         return n
 
@@ -18,7 +18,21 @@ def convert(fraction):
 
 
 def gauge(percentage):
-    ...
+
+        if 1 < n < 99:
+        print(str(n)+"%")
+        break
+
+        elif n == 0 or n == 1:
+            print("E")
+            break
+
+        elif n == 99 or n == 100:
+            print("F")
+            break
+
+        elif n > 100:
+            pass
 
 
 if __name__ == "__main__":
