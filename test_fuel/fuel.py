1b898060c5e198318f8837a9a1a7bfe3f86c5828
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 05:31:57 +0000

    Wed, Jun 15, 2022, 5:31 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,3 +1,5 @@
+import sys
+
 def main():
     frac = input("Fraction:")
     conv = convert(frac)
@@ -5,17 +7,19 @@ def main():
 
 
 def convert(fraction):
-     n = 0
-     try:
-        split = fraction.split("/")
-        n = n + (round(((int(split[0]))/(int(split[1])))*100))
-        return n
 
-    except ZeroDivisionError as e:
-        pass
+    while True:
+        n = 0
+        try:
+            split = fraction.split("/")
+            n = n + (round(((int(split[0]))/(int(split[1])))*100))
+            return n
 
-    except ValueError as e:
-        pass
+        except ZeroDivisionError as e:
+            sys.exit()
+
+        except ValueError as e:
+            sys.exit()
 
 
 
@@ -23,15 +27,14 @@ def gauge(percentage):
 
     if 1 < n < 99:
         return f"{str(n)}%"
-        break
 
     elif n == 0 or n == 1:
         return "E"
-        break
+
 
     elif n == 99 or n == 100:
         return "F"
-        break
+
 
 if __name__ == "__main__":
     main()
