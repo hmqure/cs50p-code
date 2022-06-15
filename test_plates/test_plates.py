453df730db514a42a3d6a5cbba8f27e74ab4e682
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 01:09:53 +0000

    Wed, Jun 15, 2022, 1:09 AM +00:00

---
+++ test_plates/test_plates.py
@@ -0,0 +1,22 @@
+from bank import value
+
+def main():
+    test_hello()
+    test_h()
+    test_non()
+
+def test_hello():
+    assert value("Hello") == 0
+    assert value("hello") == 0
+
+def test_h():
+    assert value("hey") == 20
+    assert value("Hopper") == 20
+
+def test_non():
+    assert value("very") == 100
+    assert value("3453") == 100
+    assert value("?>/.") == 100
+
+if __name__ == "__main__":
+    main()
