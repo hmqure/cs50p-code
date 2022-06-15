6efc619bd6a7cdef7e1e242152a0a96cd67da432
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 05:37:13 +0000

    Wed, Jun 15, 2022, 5:37 AM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,17 +1,19 @@
-from twttr import shorten
+from fuel import convert, gauge
 
 def main():
-    test_shorten()
+    test_convert()
+    test_gauge()
 
 
 
-def test_shorten():
-    assert shorten('hello') == 'hll'
+def test_convert():
+    assert convert('hello') == 'hll'
     assert shorten('Hello') == 'Hll'
     assert shorten('HELLO') == 'HLL'
-    assert shorten('123') == '123'
-    assert shorten('hello123') == 'hll123'
-    assert shorten('hello, world') == 'hll, wrld'
+
+def test_gauge():
+    assert shorten('hello') == 'hll'
+    assert shorten('Hello') == 'Hll'
 
 if __name__ == "__main__":
     main()
