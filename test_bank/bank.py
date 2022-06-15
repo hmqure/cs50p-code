0e26560cfdeed3f714796b7c1d22f1237df2148e
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 00:47:02 +0000

    Wed, Jun 15, 2022, 12:47 AM +00:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,9 +1,18 @@
 def main():
-    ...
+    str = input("Greeting: ")
 
 
 def value(greeting):
-    ...
+    low = str.lower()
+    low = low.replace(" ", "")
+
+    if low[0] == "h":
+        if low[0:5] == "hello":
+            print("$0")
+        else:
+            print("$20")
+    else:
+        print("$100")
 
 
 if __name__ == "__main__":
