d3b083c2b13f4a83f31f2efb184033de01415730
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 04:17:16 +0000

    Wed, Jun 15, 2022, 4:17 AM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -2,6 +2,9 @@ from plates import is_valid
 
 def main():
     test_len()
+    test_alph()
+    test_zero()
+    test_alnum()
 
 def test_len():
     assert is_valid("G") is not True
@@ -14,10 +17,12 @@ def test_alph():
     assert is_valid('AAA22A') is not True
     assert is_valid('AAA222') is True
 
-def test_alph():
-    assert is_valid('2AAAA') is not True
-    assert is_valid('AAA22A') is not True
-    assert is_valid('AAA222') is True
+def test_zero():
+    assert is_valid('AAA022') is not True
+
+def test_alnum():
+    assert is_valid('AAA./2') is not True
+
 
 if __name__ == "__main__":
     main()
