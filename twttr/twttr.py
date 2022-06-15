0ee7d4814a9106eb25df6a33a832c9926dc16761
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 00:35:35 +0000

    Wed, Jun 15, 2022, 12:35 AM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,20 +1,16 @@
-def main():
-    enter = input("Input:")
-    print(shorten(enter))
+
+enter = input("Input:")
+
+new = ''
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+lower = word.lower()
+
+for i in range(len(lower)):
+    if lower[i] not in vowels:
+        new += lower[i]
+
+print(new)
 
 
-def shorten(word):
-    new = ''
-    vowels = ['a', 'e', 'i', 'o', 'u']
 
-    lower = word.lower()
-
-    for i in range(len(lower)):
-        if lower[i] not in vowels:
-            new += lower[i]
-
-    return new
-
-
-if __name__ == "__main__":
-    main()
