51e037bc6f89ddde564796f21ffc3ede05f2a492
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 00:54:39 +0000

    Wed, Jun 15, 2022, 12:54 AM +00:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,7 +1,15 @@
+from bank import value
+
 def main():
+    test_value()
 
-
-def test_value(greeting):
+def test_value():
+    assert value("Hello") == "$0"
+    assert value("hello") == "$0"
+    assert value("hey") == "$20"
+    assert value("Hopper") == "$20"
+    assert value("Hello") == "$0"
+    assert value("Hello") == "$0"
 
 
 
