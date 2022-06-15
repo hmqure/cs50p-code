d9d7dab64761d72e610d9d220220d84bfdeed6a9
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-06-15 05:21:08 +0000

    Wed, Jun 15, 2022, 5:21 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -3,7 +3,11 @@ def main():
 
 
 def convert(fraction):
-    
+     n = 0
+     try:
+        split = frac.split("/")
+        n = n + (round(((int(split[0]))/(int(split[1])))))
+
 
 
 def gauge(percentage):
