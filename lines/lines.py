024561c1652f5d272543d915c49cc23a2e14e21a
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-09-21 18:00:23 +0000

    Wed, Sep 21, 2022, 6:00 PM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -6,13 +6,16 @@ line_list = []
 
 if len(sys) > 2:
     print("Too many command-line arguments")
+    sys.exit()
 
 elif len(sys) == 1:
     print("Too few command-line arguments")
+    sys.exit()
 
 elif len(sys) == 2:
     if (sys[1][-3::]) != ".py":
         print("Not a python file")
+        sys.exit()
 
     else:
         with open(sys[1], "r") as file:
