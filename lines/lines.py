c0913ea176c28e2980134451e3032d73fab22787
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-09-21 17:44:57 +0000

    Wed, Sep 21, 2022, 5:44 PM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -3,3 +3,12 @@ import sys
 sys = sys.argv
 
 print(sys)
+
+if len(sys) > 2:
+    print("Too many command-line arguments")
+
+elif len(sys) == 1:
+    print("Too few command-line arguments")
+
+elif len(sys) == 2:
+    if
