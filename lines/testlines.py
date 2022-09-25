d0bfe7c42dd42397a80aa6790c6c66a15f0d9370
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-09-25 10:21:18 +0000

    Sun, Sep 25, 2022, 10:21 AM +00:00

--- lines/testlines.py
+++ lines/testlines.py
@@ -1,17 +1,14 @@
 hello = "hello"
 
 #comment
-#dd
-#ddd
-#Dddd
+
 
 print(hello)
-     print(hello)
+     
 print(hello)
 print(hello)
 print(hello)
 
 """
 dsdsdfsdfsdfsdf
-
-                """
+"""
