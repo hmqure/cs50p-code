02d2f9db002b953998d73768fdedee7edd97045b
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-09-25 10:53:36 +0000

    Sun, Sep 25, 2022, 10:53 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,4 +1,29 @@
 import tabulate
+import sys
+
+if len(syst) > 2:
+    sys.exit("Too many command-line arguments")
+
+elif len(syst) == 1:
+    sys.exit("Too few command-line arguments")
+
+elif len(syst) == 2:
+    if (syst[1][-3::]) != ".py":
+        sys.exit("Not a python file")
+
+    else:
+        try:
+            with open(syst[1], "r") as file:
+                line = file.readlines()
+                line_list.append(line)
+
+                for i in line_list[0]:
+                    if not i.lstrip().startswith('#') and i.lstrip() != '':
+                        fin_list.append(i)
+
+        except FileNotFoundError:
+            sys.exit("File does not exist")
+
 
 file = 'sicilian.csv'
 
