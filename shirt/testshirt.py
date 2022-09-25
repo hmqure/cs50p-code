2f3896057cfc1d246250945e537d20f417f963f9
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-09-25 12:08:50 +0000

    Sun, Sep 25, 2022, 12:08 PM +00:00

--- shirt/testshirt.py
+++ shirt/testshirt.py
@@ -2,13 +2,13 @@ from tabulate import tabulate
 import sys
 import csv
 
-syst = sys.argv
+syst = ['1.png', 'dsdg.jpg']
 extensions = ['jpg','jpeg','png']
 
 sys1ext = []
 sys2ext = []
 
-syst[1].split(".")
+print(syst[1].split("."))
 sys1ext = syst[-1]
 
-print(sys1exit)
+print(sys1ext)
