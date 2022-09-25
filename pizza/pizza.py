f6c0d14286340c3e1fd42437e1cb8eec6301ef62
hmqure1 <107221865+hmqure1@users.noreply.github.com>
2022-09-25 10:48:26 +0000

    Sun, Sep 25, 2022, 10:48 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,21 +1,23 @@
-# file = 'sicilian.csv'
-
-# with open(file, 'r') as file:        #open tab file
-#     menu = file.readlines()
-
-# stripped = []
-
-# for items in menu:
-#     strip = (items.strip('\n'))
-#     stripped.append(strip)#.split('\t'))
-
-# splitted = []
-
-# for i in stripped:
-#     splitted.append(i.split(','))
-
-# print(splitted)
-
 import tabulate
 
+file = 'sicilian.csv'
+
+with open(file, 'r') as file:        #open tab file
+    menu = file.readlines()
+
+stripped = []
+
+for items in menu:
+    strip = (items.strip('\n'))
+    stripped.append(strip)#.split('\t'))
+
+splitted = []
+
+for i in stripped:
+    splitted.append(i.split(','))
+
+print(splitted)
+
 print(tabulate.grid('sicilian.csv'))
+
+print(tabulate(mydata, headers=head, tablefmt="grid"))
